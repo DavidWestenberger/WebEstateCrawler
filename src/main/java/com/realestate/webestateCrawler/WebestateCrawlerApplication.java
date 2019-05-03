5e8a550d9daf1121ae3996178761fe025c3c2086
david <davidwestenberger@gmail.com>
2019-05-02 22:10:46 -0500

    first initial commit

---
+++ src/main/java/com/realestate/webestateCrawler/WebestateCrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.realestate.webestateCrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebestateCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebestateCrawlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/realestate/webestateCrawler/WebestateCrawlerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.realestate.webestateCrawler;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebestateCrawlerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
